f6c27665377c9af321fc1bb993843045439194a9
Uday Joshi <udayjoshi82@gmail.com>
2023-11-14 16:06:01 -0500

    question 1

---
+++ Round/round1.py
@@ -0,0 +1,8 @@
+"""
+1. Function to Summarize a Text File: Write a function that accepts a filename as a parameter, reads the file, and returns the number of lines, words, and characters in the file.    
+    
+"""
+def summarize_text_file(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        
